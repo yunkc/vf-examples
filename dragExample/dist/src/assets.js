39514571e40743d2321868be7a5e2edcacc2d266
wanghaikuo <wanghaikuo@vipkid.com.cn>
2020-06-12 20:34:59 +0800

    拖拽example

---
+++ dragExample/dist/src/assets.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const store_1 = require("./store");
+const IVFTemplate_1 = require("../types/IVFTemplate");
+const assets = {
+    [store_1.Ids.dinoImage]: {
+        type: IVFTemplate_1.AssetType.image,
+        url: "https://vipkid-edu.github.io/vf-gui/play/assets/dino.png",
+        name: store_1.Ids.dinoImage.toString()
+    },
+    [store_1.Ids.drag]: {
+        type: IVFTemplate_1.AssetType.image,
+        url: './assets/mouse.png',
+        name: store_1.Ids.dinoImage.toString()
+    },
+};
+exports.default = assets;
---
+++ dragExample/src/store.ts
@@ -0,0 +1,17 @@
+export enum Ids {
+    root,
+    dinoImage,
+    title,
+    drag,
+    rect,
+    childContainer
+}
+
+export namespace App {
+    export const name = 'VFT2J';
+    export const width = 890;
+    export const height = 500;
+    export const baseUrl = '';
+    export const conversion = '';
+    export const version = '0.0.1';
+}
