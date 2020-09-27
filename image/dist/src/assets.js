73e334811f3a9bbbfd18f1187d9d785cf1bdd437
wanghaikuo <wanghaikuo@vipkid.com.cn>
2020-09-27 19:15:12 +0800

    资源url更换

--- image/dist/src/assets.js
+++ image/dist/src/assets.js
@@ -5,27 +5,27 @@ const IVFTemplate_1 = require("../types/IVFTemplate");
 const assets = {
     [store_1.Ids.dinoImage]: {
         type: IVFTemplate_1.AssetType.image,
-        url: 'https://vipkid-edu.github.io/vf-gui/play/assets/dino.png',
+        url: 'https://vipkid-edu.github.io/vf-examples/assets/dino.png',
         name: store_1.Ids.dinoImage.toString()
     },
     [store_1.Ids.pcImage]: {
         type: IVFTemplate_1.AssetType.image,
-        url: 'https://vipkid-edu.github.io/vf-gui/play/assets/pc.png',
+        url: 'https://vipkid-edu.github.io/vf-examples/assets/pc.png',
         name: store_1.Ids.pcImage.toString()
     },
     [store_1.Ids.bg2]: {
         type: IVFTemplate_1.AssetType.image,
-        url: 'https://vipkid-edu.github.io/vf-gui/play/assets/mask/bg2.png',
+        url: 'https://vipkid-edu.github.io/vf-examples/assets/mask/bg2.png',
         name: store_1.Ids.bg2.toString()
     },
     [store_1.Ids.clear1]: {
         type: IVFTemplate_1.AssetType.image,
-        url: 'https://vipkid-edu.github.io/vf-gui/play/assets/mask/clear1.png',
+        url: 'https://vipkid-edu.github.io/vf-examples/assets/mask/clear1.png',
         name: store_1.Ids.clear1.toString()
     },
     [store_1.Ids.bg1]: {
         type: IVFTemplate_1.AssetType.image,
-        url: 'https://vipkid-edu.github.io/vf-gui/play/assets/mask/bg.jpg',
+        url: 'https://vipkid-edu.github.io/vf-examples/assets/mask/bg.jpg',
         name: store_1.Ids.bg1.toString()
     }
 };
--- image/src/assets.ts
+++ image/src/assets.ts
@@ -1,30 +1,30 @@
-import {Ids} from "./store";
-import {Assets, AssetType} from '../types/IVFTemplate'
+import { Ids } from "./store";
+import { Assets, AssetType } from '../types/IVFTemplate'
 
 const assets: Assets = {
     [Ids.dinoImage]: {
         type: AssetType.image,
-        url: 'https://vipkid-edu.github.io/vf-gui/play/assets/dino.png',
+        url: 'https://vipkid-edu.github.io/vf-examples/assets/dino.png',
         name: Ids.dinoImage.toString()
     },
     [Ids.pcImage]: {
         type: AssetType.image,
-        url: 'https://vipkid-edu.github.io/vf-gui/play/assets/pc.png',
+        url: 'https://vipkid-edu.github.io/vf-examples/assets/pc.png',
         name: Ids.pcImage.toString()
     },
     [Ids.bg2]: {
         type: AssetType.image,
-        url: 'https://vipkid-edu.github.io/vf-gui/play/assets/mask/bg2.png',
+        url: 'https://vipkid-edu.github.io/vf-examples/assets/mask/bg2.png',
         name: Ids.bg2.toString()
     },
     [Ids.clear1]: {
         type: AssetType.image,
-        url: 'https://vipkid-edu.github.io/vf-gui/play/assets/mask/clear1.png',
+        url: 'https://vipkid-edu.github.io/vf-examples/assets/mask/clear1.png',
         name: Ids.clear1.toString()
     },
     [Ids.bg1]: {
         type: AssetType.image,
-        url: 'https://vipkid-edu.github.io/vf-gui/play/assets/mask/bg.jpg',
+        url: 'https://vipkid-edu.github.io/vf-examples/assets/mask/bg.jpg',
         name: Ids.bg1.toString()
     }
 }
